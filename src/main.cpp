55f16aee1a4d8d1d8beeb8094216c6f8834120e9
km <169460914+v49480nd-km@users.noreply.github.com>
2024-06-10 21:40:21 -0400

    able to draw triangle

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,36 @@
 #include <cstdio>
-#include <raylib.h>
 
-int
-main(void)
-{
-    const int WIDTH = 800;
-    const int HEIGHT = 600;
+#include "raylib.h"
 
-    InitWindow(WIDTH, HEIGHT, "CALAXIAN");
-    SetTargetFPS(60);
+#define FONT "../data/fonts/Warzone.ttf"
+#define FPS 60
+#define SCREEN_WIDTH 800
+#define SCREEN_HEIGHT 600
+
+int main(void) {
+    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "CALAXIAN");
+    SetTargetFPS(FPS);
+    Font warzone = LoadFont(FONT);
+    Vector2 fontPosition = { 0, 0 };
+    Vector2 triOne = {
+        (SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 3)
+    };
+    Vector2 triTwo = {
+        (SCREEN_WIDTH / 3), (SCREEN_HEIGHT - (SCREEN_HEIGHT / 3))
+    };
+    Vector2 triThree = {
+        (SCREEN_WIDTH - (SCREEN_WIDTH / 3)),
+        (SCREEN_HEIGHT - (SCREEN_HEIGHT / 3))
+    };
 
     while (!WindowShouldClose()) {
         BeginDrawing();
             ClearBackground(RAYWHITE);
-            DrawText("CALAXIAN", 190, 200, 84, BLACK);
+            DrawTextEx(warzone, "Calaxian", fontPosition, 42, 2, BLACK);
+            DrawTriangle(triOne, triTwo, triThree, BLACK);
         EndDrawing();
     }
 
     CloseWindow();
-
     return 0;
 }
